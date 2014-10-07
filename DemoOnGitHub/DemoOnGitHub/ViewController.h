b04ff51dd47d4408c61b9dddca5bee811950f9df
sheshnath <sheshnath.iicmr@gmail.com>
2014-10-07 12:06:55 +0530

    initial commit
    project for just demo

---
+++ DemoOnGitHub/DemoOnGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoOnGitHub
+//
+//  Created by Zensar Technologies3 on 07/10/14.
+//  Copyright (c) 2014 Zensar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoOnGitHub/DemoOnGitHub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DemoOnGitHub
+//
+//  Created by Zensar Technologies3 on 07/10/14.
+//  Copyright (c) 2014 Zensar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
